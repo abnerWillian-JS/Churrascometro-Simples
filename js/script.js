7b28b39f1954dcea4bdf50c234acadc33af581ca
abnerWillian-JS <121897355+abnerWillian-JS@users.noreply.github.com>
2023-01-03 17:58:49 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ let qtdTotalCarne = adultos * 400 + (200 * crianças)
 let bebedeira = cerveja * 5
 let refri = (adultos * 500) + (crianças * 200)
 
-resultado.innerHTML = '<p> <img src="/css/images/CHURRAS.png" alt="EspetoDeChurrasco" width="30px">' + (qtdTotalCarne) + 'g de carne </p>'
-resultado.innerHTML +=  '<p> <img src="/css/images/cerveja.png" alt="Cerveja" width="30px">' + (bebedeira) + ' garrafas de cerveja </p>'
-resultado.innerHTML += '<p> <img src="/css/images/REFRI.png" alt="Refrigerante" width="30px">' + (refri) + 'ml de refrigerante </p>'
+resultado.innerHTML = '<p> <img src="css/images/CHURRAS.png" alt="EspetoDeChurrasco" width="30px">' + (qtdTotalCarne) + 'g de carne </p>'
+resultado.innerHTML +=  '<p> <img src="css/images/cerveja.png" alt="Cerveja" width="30px">' + (bebedeira) + ' garrafas de cerveja </p>'
+resultado.innerHTML += '<p> <img src="css/images/REFRI.png" alt="Refrigerante" width="30px">' + (refri) + 'ml de refrigerante </p>'
 }
